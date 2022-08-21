ca315cd7e621ae728053309c96daf6d36ee75ae3
Liubomyr <64606455+keijnn@users.noreply.github.com>
2022-08-21 17:55:18 +0300

    Update Profile.tsx

--- src/components/side-bar/header/profile-and-search/Profile.tsx
+++ src/components/side-bar/header/profile-and-search/Profile.tsx
@@ -2,6 +2,6 @@ import ProfileAvatar from '@/assets/icons/profile-user.svg'
 
 export const Profile = () => (
   <div>
-    <img className="w-1/12" src={ProfileAvatar} alt="ava" />
+    <img className="w-1/12 mb-3 mt-3" src={ProfileAvatar} alt="ava" />
   </div>
 )
